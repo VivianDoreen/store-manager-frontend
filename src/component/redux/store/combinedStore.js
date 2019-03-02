af8b72cdb4e1309257c3e34bd8e91c3bc49216af
VivianDoreen <nabulovivian2014@gmail.com>
2019-03-02 15:38:11 +0300

    chore(setup-redux): Setting up redux for the project
     - Added the redux dependency to package.json
     - Created the redux file structure
    
    [Maintains #164349614]

---
+++ src/component/redux/store/combinedStore.js
@@ -0,0 +1,10 @@
+import { applyMiddleware, createStore } from 'redux';
+import mainReducer from '../reducers/mainReducer.js';
+import thunk from "redux-thunk";
+import { composeWithDevTools } from "redux-devtools-extension";
+
+const store = createStore(
+    mainReducer, 
+    composeWithDevTools(applyMiddleware(thunk)));
+
+export default store;
