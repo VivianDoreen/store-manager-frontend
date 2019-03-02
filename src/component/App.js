37843d1efd1cb3af38de9690e32b85ca2ea7d6ca
VivianDoreen <nabulovivian2014@gmail.com>
2019-03-02 23:14:01 +0300

    Chore(TestEnvironment): Set up the frontend test environment
    - Install Jest test library
    - Set up jest in package.json
    - Install Enzyme test utility
    - Set up Enzyme in package.json
    - Install cypress
    - Set up cypress in package.json
    
    [Maintains #164349685]

---
+++ src/component/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import '../css/main.css'
+
+export class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Store Manager</h1>
+        <p id="welcome_text">Welcome to Store Manager</p>
+      </div>
+    );
+  }
+}
+
+export default App;
